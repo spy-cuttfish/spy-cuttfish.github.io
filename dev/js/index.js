f48982922014010713a0b3f75d5cc396beb6e289
spy-cuttfish <62491092+spy-cuttfish@users.noreply.github.com>
2021-03-29 00:45:01 +0800

    change nightmode
    夜间模式修改

--- dev/js/index.js
+++ dev/js/index.js
@@ -12,11 +12,11 @@ $(document).ready(function(){
         });
         e.stopPropagation();
     });
-
+    
     nav.on('click', function(e) {
         e.stopPropagation();
     });
-
+    
     /*
     *  Header Bar
     */
@@ -27,11 +27,11 @@ $(document).ready(function(){
         var navText = nav.find('a');
         var themeStyle = $('.g-banner').attr('data-theme');
         var scFlag = $(document).scrollTop();
-
+    
         $(document).scroll(function() {
             var scrollTop = $(this).scrollTop();
             var navClassName = 'nav-' + themeStyle;
-
+    
             if (scrollTop > headerHeight) {
                 if(scrollTop > 3 * headerHeight) {
                     header.addClass('headerUp');
@@ -59,7 +59,7 @@ $(document).ready(function(){
                 navText.css('color', '#fff');
                 nav.removeClass(navClassName);
             }
-
+    
             // scroll action
             if (scFlag > scrollTop) {
                 header.addClass('headerDown');
@@ -69,7 +69,7 @@ $(document).ready(function(){
             scFlag = scrollTop;
         });
     }
-
+    
     /*
     * Post Cover Resize
     */
@@ -78,7 +78,7 @@ $(document).ready(function(){
         var containerWidth = container.width();
         var imgHeight = img.height();
         var containerHeight = container.height();
-
+    
         if (imgHeight < containerHeight) {
             img.css({
                 'width': 'auto',
@@ -92,10 +92,10 @@ $(document).ready(function(){
             var marginTop = (containerHeight - imgHeight) / 2;
             img.css('margin-top', marginTop + 'px');
         }
-
+    
         img.fadeIn();
     }
-
+    
     /**
      * The Post Navigator
      */
@@ -105,11 +105,11 @@ $(document).ready(function(){
         $(this).css('margin-top', (rn - n) / 2 + 'px');
         $(this).fadeIn();
     });
-
+    
     $('.read-next-item img').each(function(){
         postCover($(this), $('.read-next-item'));
     });
-
+    
     /**
      * Pagination
      */
@@ -118,9 +118,9 @@ $(document).ready(function(){
         var current = parseInt($('#current_pages').val());
         var baseUrl = $('#base_url').val();
         var limit = 3;
-
+    
         var link_html = '';
-
+    
         for (var i = current - limit; i < current; i++) {
             if (i > 0 && i !== 1) {
                 link_html += '<a href="' + baseUrl + 'page' + i + '" class="page-link page-num">' + i + '</a>';
@@ -128,19 +128,19 @@ $(document).ready(function(){
                 link_html += '<a href="' + baseUrl + '" class="page-link page-num">' + i + '</a>';
             }
         }
-
+    
         link_html += '<span class="page-link page-num active">' + current + '</span>';
-
+    
         for (var j = current + 1; j <= current + limit; j++) {
             if (j <= total) {
                 link_html += '<a href="' + baseUrl + 'page' + j + '" class="page-link page-num">' + j + '</a>';
             }
         }
-
+    
         $('#page-link-container').html(link_html);
     }
     pagination();
-
+    
     /**
      * Search
      */
@@ -148,14 +148,14 @@ $(document).ready(function(){
         var self = this;
         var input = $('#search_input');
         var result = $('.search_result');
-
+    
         input.focus(function() {
             $('.icon-search').css('color', '#3199DB');
             result.show();
         });
-
+    
         input.keyup(debounce(this.autoComplete));
-
+    
         $(document).click(function(e) {
             if(e.target.id === 'search_input' || e.target.className === 'search_result' || e.target.className === 'search_item') {
                 return;
@@ -164,16 +164,16 @@ $(document).ready(function(){
             result.hide();
         });
     }
-
+    
     Search.prototype.autoComplete = function() {
         var keywords = this.value.toLowerCase();
-
+    
         if (keywords.length) {
             $('.icon-search').css('color', '#3199DB');
         } else {
             $('.icon-search').css('color', '#CAD3DC');
         }
-
+    
         $.getJSON('../../search.json').done(function(data) {
             var html = '';
             for (var i in data) {
@@ -181,7 +181,7 @@ $(document).ready(function(){
                 var title = item.title;
                 var tags = item.tags;
                 var url = item.url;
-
+    
                 var k = title + tags;
                 if (keywords !== '' && k.toLowerCase().indexOf(keywords) >= 0) {
                     html += '<a class="search_item" href="' + item.url + '">' + item.title + '</a>';
@@ -190,11 +190,11 @@ $(document).ready(function(){
             $('.search_result').html(html);
         });
     };
-
+    
     function debounce(fn, delay) {
         var timer;
         delay = delay || 120;
-
+    
         return function () {
             var ctx = this;
             var args = arguments;
@@ -205,28 +205,28 @@ $(document).ready(function(){
             timer = setTimeout(later, delay);
         };
     }
-
+    
     new Search();
-
+    
     /**
      * Night mode
      */
     function nightMode() {
         var el = $('body');
         var className = 'night-mode';
-
+    
         var date = new Date();
         var hour = date.getHours();
-
-        if (hour <= 6 || hour >= 2) {
+    
+        if (hour <= 6 && hour >= 2) {
             el.addClass(className);
         }
     }
-
+    
     if ($('#nm-switch').val() === 'true') {
         nightMode();
     }
-
+    
     /**
      * Copy and copyright
      */
